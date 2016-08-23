479da3604136c13312257edbd61828487aab7354
DamonYoung42 <damonyoung@wi.rr.com>
2016-08-23 12:35:55 -0500

    custom class add/remove functions.

---
+++ CustomListClass/GenericList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    public class GenericList<T> : IEnumerable<T>
+    {
+        private T[] initialList;
+
+        public GenericList(T[] size)
+        {
+            initialList = size;
+
+
+        }
+        public void Add(T itemToAdd)
+        {
+            T[] newArray = new T[initialList.Length + 1];
+            for (int i = 0; i < initialList.Length; i++)
+            {
+                newArray[i] = initialList[i];
+            }
+            newArray[newArray.Length - 1] = itemToAdd;
+            initialList = newArray;
+        }
+
+        public void Remove(T itemToRemove)
+        {
+            T[] newArray = new T[initialList.Length - 1];
+            for (int i = 0; i < initialList.Length - 1; i++)
+            {
+                newArray[i] = initialList[i];
+            }
+            initialList = newArray;
+        }
+
+        public IEnumerator<T[]> GetEnumerator()
+        {
+            Console.WriteLine("T[].GetEnumerator");
+            for (int i = 0; i < initialList.Count(); i++)
+            {
+                yield return initialList;
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Console.WriteLine("<T>.GetEnumerator");
+            for (int i = 0; i < initialList.Count(); i++)
+            {
+                yield return initialList[i];
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            Console.WriteLine("GetEnumerator");
+            for (int i = 0; i < initialList.Count(); i++)
+            {
+                yield return initialList[i];
+            }
+        }
+    }
+}
---
+++ CustomListClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            GenericList<string> stringList = new GenericList<string>(new string[] { "name", "city", "address", "state", "age" });
+            
+
+            stringList.Add("country");
+            stringList.Add("dob");
+
+            stringList.Remove("age");
+            stringList.Remove("city");
+
+            Console.ReadLine();
+            //numberList.Add(itemToAdd);
+            //Console.WriteLine(stringList);
+            //Console.ReadKey();
+            //itemToRemove = 2;
+            //nameList.Remove(itemToRemove);
+        }
+    }
+}
